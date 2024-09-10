9f96dfe6363131dc26029f1710528648069578a7
Abdelrahman <abdofathi317@gmail.com>
2024-09-10 11:46:33 -0700

    user in app file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,5 +17,18 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/', methods=['POST'])
+def users() -> str:
+    """Route to user page"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsomify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
