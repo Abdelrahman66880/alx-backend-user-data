70235620c8e3c0090b387898142dcb21c52870c0
Abdelrahman <abdofathi317@gmail.com>
2024-09-11 09:03:34 -0700

    Destroy the user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,3 +82,13 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: str) -> str:
+        """destroy the user with crossponding user id"""
+        try:
+            user = self._db.find_user_by(user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
